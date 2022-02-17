fa3d00d654c5bcb967e7fd4c3a12083e8fb11ca0
HEMANTHESWARREDDY <86761812+HEMANTHESWARREDDY@users.noreply.github.com>
2022-02-18 01:15:30 +0530

    guessme

---
+++ GUESS ME/script.js
@@ -0,0 +1,12 @@
+function previous(){
+    document.getElementById("myImage")
+    .src="211FA18077.JPG"
+    document.getElementById("message")
+    .innerHTML="Halamithi Habibo"
+}
+function next(){
+    document.getElementById("myImage")
+    .src="211FA18072.JPG"
+    document.getElementById("message")
+    .innerHTML="BYE! BOOBY"
+} 
